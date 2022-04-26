3f52aca6fef4ccf69bf597e4c68eb3f62b6c6a18
Ravin Gunasekara <ravin.ucsc@gmail.com>
2022-04-26 21:28:42 +0530

    Bug fixes and market data query

--- src/transaction_data/transaction_data_config.py
+++ src/transaction_data/transaction_data_config.py
@@ -1,5 +1,6 @@
 message_to_endpoint_mapping = {
     "PositionUpdate": "position-api/position-updates",
     "Position": "position-api/positions",
-    "RealtimeRiskFactorUpdate": "market-data-api/realtime-risk-factor-updates"
+    "RealtimeRiskFactorUpdate": "market-data-api/realtime-risk-factor-updates",
+    "Realtime_Risk_Factor_Values": "market-data-api/realtime-risk-factor-values"
 }
