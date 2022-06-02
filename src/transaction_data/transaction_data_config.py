2844a792e1134352bded62707f5783c43827c285
Ravin Gunasekara <ravin.ucsc@gmail.com>
2022-06-02 20:41:22 +0530

    API level changes and stress testing related changes

--- src/transaction_data/transaction_data_config.py
+++ src/transaction_data/transaction_data_config.py
@@ -1,7 +1,10 @@
 message_to_endpoint_mapping = {
     "Position Update": "position-api/position-updates",
-    "Position": "position-api/positions",
+    "Position": "position-api/positions-search",
     "Realtime Risk Factor Update": "market-data-api/realtime-risk-factor-updates",
     "Realtime Risk Factor Value": "market-data-api/realtime-risk-factor-values",
-    "Hedge Efficiency": "margin-value-api/hedge-efficiency"
+    "Hedge Efficiency": "margin-value-api/hedge-efficiency",
+    "Realtime Interest Curve Value": "market-data-api/realtime-interest-curve-values",
+    "Stress Test": "margin-value-api/stress-tests",
+    "Stress Test Result": "margin-value-api/stress-test-results"
 }
